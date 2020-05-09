e1afa5266a052a7fb85f34859a579365fe21f737
BaherAbdelfatah <baherbeho_ls@yahoo.com>
2020-05-09 06:14:37 +0200

    Styling the marker

--- app/src/main/java/com/baherandroid/wander/MapsActivity.kt
+++ app/src/main/java/com/baherandroid/wander/MapsActivity.kt
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
@@ -95,6 +96,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                             .position(latLng)
                             .title(getString(R.string.dropped_pin))
                             .snippet(snippet)
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
 
             )
         }
